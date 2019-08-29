ed685115eb1bd6dd0875741e498a08fb073c2fc6
Paul Melnikow <github@paulmelnikow.com>
2019-08-29 12:24:34 -0400

    Set up Prettier (#3)

--- README.md
+++ README.md
@@ -1,20 +1,15 @@
-matrix-inverse-js
-=================
+# matrix-inverse-js
 
 Matrix inverse function packaged for CommonJS. Code is from [sylvester][]
 by [James Coglan][], with gratitude.
 
-
-Installation
-------------
+## Installation
 
 Install matrix-inverse-js by running:
 
     npm install matrix-inverse
 
-
-Example
--------
+## Example
 
     var matrixInverse = require('matrix-inverse');
 
@@ -25,26 +20,19 @@ Example
 
     var M_inv = matrixInverse(M);
 
-
-Origin
-------
+## Origin
 
 This code was copied and adapted from sylvester at commit
 5a2c61681e988d60bf0a4223640c636052946341.
 
-
-Contribute
-----------
+## Contribute
 
 - Issue Tracker: github.com/bodylabs/matrix-inverse-js/issues
 - Source Code: github.com/bodylabs/matrix-inverse-js
 
-
-License
--------
+## License
 
 The project is licensed under the MIT license.
 
-
 [sylvester]: https://github.com/jcoglan/sylvester
-[James Coglan]: http://jcoglan.com/
+[james coglan]: http://jcoglan.com/
--- test.js
+++ test.js
@@ -1,27 +1,20 @@
 var chai = require('chai'),
-    chaiStats = require('chai-stats');
+  chaiStats = require('chai-stats')
 
-chai.use(chaiStats);
+chai.use(chaiStats)
 
 var should = chai.should(),
-    expect = chai.expect;
+  expect = chai.expect
 
+var matrixInverse = require('./matrix-inverse')
 
-var matrixInverse = require('./matrix-inverse');
+var M = [[3, 3.2], [3.5, 3.6]]
 
-var M = [
-    [3, 3.2],
-    [3.5, 3.6],
-];
+var M_inv = matrixInverse(M)
 
-var M_inv = matrixInverse(M);
+var expected = [[-9, 8], [8.75, -7.5]]
 
-var expected = [
-    [-9, 8],
-    [8.75, -7.5],
-];
-
-expect(M_inv[0][0]).to.almost.equal(expected[0][0]);
-expect(M_inv[0][1]).to.almost.equal(expected[0][1]);
-expect(M_inv[1][0]).to.almost.equal(expected[1][0]);
-expect(M_inv[1][1]).to.almost.equal(expected[1][1]);
+expect(M_inv[0][0]).to.almost.equal(expected[0][0])
+expect(M_inv[0][1]).to.almost.equal(expected[0][1])
+expect(M_inv[1][0]).to.almost.equal(expected[1][0])
+expect(M_inv[1][1]).to.almost.equal(expected[1][1])
